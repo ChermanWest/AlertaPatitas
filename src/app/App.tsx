beee2587b4474b1d59f7816326cd9989e31d3556
German Castro Torres <german.17.ct@gmail.com>
2026-06-01 19:56:00 -0400

    se añaden recursos visuales de la pagina

--- src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-import { HeroBanner } from "./components/HeroBanner";
-
-export default function App() {
-  return (
-    <div className="bg-white relative size-full">
-      <HeroBanner />
-    </div>
-  );
-}
